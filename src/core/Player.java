a84178a438e083bc2948ede9410798ab8366daf3
Richy44 <richardwyss@gmail.com>
2020-04-07 14:38:54 +0200

    Additional Player Constructor and changing participant into array in Tournament

--- src/core/Player.java
+++ src/core/Player.java
@@ -4,10 +4,18 @@ public abstract class Player {
     public Strategy strategy;
     public PlayerPosition position;
 
+    public Player(Strategy strategy) {
+        this.strategy = strategy;
+    }
+
     public Player(Strategy strategy, PlayerPosition position) {
         this.strategy = strategy;
         this.position = position;
     }
+
+    public Strategy getStrategy() {
+		return strategy;
+	}
 
     public abstract Move move(History history);
 }
--- src/core/Tournament.java
+++ src/core/Tournament.java
@@ -1,5 +1,7 @@
 package core;
 
+import java.util.ArrayList;
+import java.util.List;
 import prisonerdilemma.PlayerPD;
 import prisonerdilemma.PlayerPositionPD;
 import prisonerdilemma.PrisonersDilemmaRule;
@@ -10,9 +12,13 @@ public enum Tournament {
 
     // TODO The tournament should be more generalized and automated
     public static void main(String[] args) {
-        Player playerA = new PlayerPD(StrategiesPD.TIT4TAT_PD.create(), PlayerPositionPD.PLAYER_A);
-        Player playerB = new PlayerPD(StrategiesPD.GREEDY_PD.create(), PlayerPositionPD.PLAYER_B);
-        Game game = new Game(playerA, playerB, new PrisonersDilemmaRule(), 10);
+       	List<Player> players = new ArrayList<Player>();
+        players.add(new PlayerPD(StrategiesPD.TIT4TAT_PD.create()));
+        players.add(new PlayerPD(StrategiesPD.GREEDY_PD.create()));
+
+        Game game = new Game(new PlayerPD(players.get(0).getStrategy(), PlayerPositionPD.PLAYER_A), 
+        						new PlayerPD(players.get(1).getStrategy(), PlayerPositionPD.PLAYER_B),
+        						 new PrisonersDilemmaRule(), 10);       	
         game.run();
     }
 }
